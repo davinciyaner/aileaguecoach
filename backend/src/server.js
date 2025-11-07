e226db3caf0efee43c3e406ddfa8d8aa25d3136a
300247 <finn.paustian@stud.th-luebeck.de>
2025-11-07 12:15:31 +0100

    # railway port

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 // Instantiate an Express Application
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Configure Express App Instance
 app.use(express.json( { limit: '50mb' } ));
