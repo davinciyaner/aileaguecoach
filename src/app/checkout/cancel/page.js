dcd65e1a56440f701108f496da4c77bc77dbab72
300247 <finn.paustian@stud.th-luebeck.de>
2025-11-07 12:00:54 +0100

    # deleted success checkout

--- src/app/checkout/cancel/page.js
+++
@@ -1,8 +0,0 @@
-export default function CheckoutCancel() {
-    return (
-        <div className="max-w-xl mx-auto mt-10 bg-gray-800 p-10 rounded-2xl text-center text-white">
-            <h2 className="text-3xl font-bold mb-4">Payment cancelled</h2>
-            <p className="text-gray-300">You have cancelled the payment. You can try again anytime.</p>
-        </div>
-    );
-}
