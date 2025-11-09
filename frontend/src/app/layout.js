bc140f637e951702fa201f3bc4cc19b1c65d4240
davinciyaner <finnolinoo@gmail.com>
2025-11-09 15:38:26 +0100

    # added analytics

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {AuthProvider} from "@/context/AuthContext";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
       <body>
       <AuthProvider>{children}</AuthProvider>
       <SpeedInsights />
+      <Analytics />
       </body>
       </html>
   );
